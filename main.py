dc0c2fb299f7e567cb0e7ef0f543ad3f6c876ff7
Ronald <vertigo88_x@yahoo.com>
2021-08-04 19:15:38 -0400

    add instant answers to results list

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 
-from src.functions import generate_suggestions
-from src.items import no_input_item, show_suggestion_items
+from src.functions import generate_suggestions, generate_instant_answer
+from src.items import no_input_item, show_suggestion_items, show_instant_answer
 
 
 class DuckDuckGoExtension(Extension):
@@ -21,14 +21,20 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_argument() or str()
         lang = extension.preferences["duckduckgo_search_language"]
+        include_instant_answer = extension.preferences["duckduckgo_include_instant_answer"] == "true"
 
         if len(query.strip()) == 0:
             return RenderResultListAction(no_input_item())
 
-        return RenderResultListAction(
-            show_suggestion_items(
-                [query] + generate_suggestions(query, lang=lang))
-        )
+        if include_instant_answer:
+            instant_answer = show_instant_answer(*generate_instant_answer(query, lang=lang))
+        suggestion_items = show_suggestion_items([query] + generate_suggestions(query, lang=lang))
+
+        return_list = list(suggestion_items)
+        if include_instant_answer and instant_answer is not None:
+            return_list.insert(0, instant_answer)
+
+        return RenderResultListAction(return_list)
 
 
 if __name__ == "__main__":
--- src/functions.py
+++ src/functions.py
@@ -5,6 +5,7 @@ from urllib.parse import urlencode
 
 SEARCH_URL = "https://duckduckgo.com/?"
 SUGGESTION_URL = "https://duckduckgo.com/ac/?"
+API_URL = "https://api.duckduckgo.com/?"
 
 url = "https://docs.python.org/3.4/howto/urllib2.html"
 
@@ -34,6 +35,35 @@ def generate_suggestions(search, lang="en-US"):
         suggestions = json.loads(url.read().decode()) or []
 
     return [s["phrase"] for s in suggestions if s.get("phrase")]
+
+
+def generate_instant_answer(search, lang="en-US"):
+    """
+    >>> generate_instant_answer("duckduckgo")
+    ('DuckDuckGo', "DuckDuckGo is an internet search engine that emphasizes protecting searchers' privacy and avoiding
+     the filter bubble of personalized search results. DuckDuckGo distinguishes itself from other search engines by not
+     profiling its users and by showing all users the same search results for a given search term. The company is based
+     in Paoli, Pennsylvania, in Greater Philadelphia and has 130 employees as of July 2021. The company name is a
+     reference to the children's game duck, duck, goose.", 'https://en.wikipedia.org/wiki/DuckDuckGo')
+    """
+    url = API_URL + urlencode(
+        {
+            "q": search,
+             "format": "json",
+             "no_redirect": 1,
+             "no_html": 1,
+             "skip_disambig": 1,
+        }
+    )
+    headers = {'Accept-Language': lang + ",en-US;q=0.7"}
+    req = Request(url, headers=headers)
+    with urlopen(req) as url:
+        response = json.loads(url.read().decode()) or {}
+    instant_item = response.get("Heading", "")
+    instant_answer = response.get("AbstractText", "")
+    instant_url = response.get("AbstractURL", "")
+
+    return instant_item, instant_answer, instant_url
 
 
 if __name__ == "__main__":
